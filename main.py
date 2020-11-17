dd0a7d86a029c04717a149687050a6d1676c4755
cornel-kim <cornelkemboi@yahoo.com>
2020-11-17 11:28:36 +0300

    first commit

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 
-# create flask routes
 @app.route('/')
-def index():
-
+def welcome():
     return render_template('index.html')
 
 
-@app.route('/results', methods=['POST'])
-def results():
-    num1 = request.form.get("num1", type=int)
-    num2 = request.form.get("num2", type=int)
-    operator = request.form.get('operator')
-    if operator == 'Add':
-        result = num1 + num2
+@app.route('/result', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int)
+    var_2 = request.form.get("var_2", type=int)
+    operation = request.form.get("operation")
+    if operation == 'Addition':
+        result = var_1 + var_2
+    elif operation == 'Subtraction':
+        result = var_1 - var_2
+    elif operation == 'Multiplication':
+        result = var_1 * var_2
+    elif operation == 'Division':
+        result = var_1 / var_2
     else:
-        result = "input valid options"
-    return render_template('result.html', result=result)
+        result = 'INVALID CHOICE'
+    entry = result
+    return render_template('result.html', entry=entry)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.debug = True
     app.run()
-
-#kotlin
