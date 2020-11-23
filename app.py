b239ea9ff3c6aa94fc484dfef20f102c1b905744
cornel-kim <cornelkemboi@yahoo.com>
2020-11-23 13:11:13 +0300

    first commit

--- app.py
+++ app.py
@@ -28,5 +28,14 @@ def contact():
         return render_template("contact.html", msg=msg)
 
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run()
