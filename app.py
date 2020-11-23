d47fae75f2bab5fda6d3606e21acf5089002dd00
cornel-kim <cornelkemboi@yahoo.com>
2020-11-23 13:03:31 +0300

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
 
@@ -8,10 +9,23 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    msg = "you are now on the contact us page"
-    return render_template("contact.html", msg=msg)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        conn = pymysql.connect("localhost", "root", "", "studentmgmt")
+        cur = conn.cursor()
+        sql = "INSERT INTO contatus (name, email, message) VALUES (%s, %s, %s) "
+        cur.execute(sql, (name, email, message))
+        conn.commit()
+        msg = "records inserted successfully"
+        return render_template("contact.html", msg=msg)
+    else:
+        msg = "no database found"
+        return render_template("contact.html", msg=msg)
 
 
 if __name__ == '__main__':
