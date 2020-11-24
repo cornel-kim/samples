c98cb6d157d1e3dfa86bfa64735ce0802d967be5
cornel-kim <cornelkemboi@yahoo.com>
2020-11-24 12:31:20 +0300

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pymysql
 
 app = Flask(__name__)
+app.secret_key = '123sdj'
 
 
 @app.route("/")
@@ -28,14 +29,86 @@ def contact():
         return render_template("contact.html", msg=msg)
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form['email']
+        gender = request.form['gender']
+        county = request.form['county']
+        password = request.form['password']
 
-@app.route("/login")
+        conn = pymysql.connect("localhost", "root", "", "studentmgmt")
+        cur = conn.cursor()
+        register_query = "INSERT INTO register(username, email, gender, county, password)" \
+                         "VALUES(%s, %s, %s, %s, %s)"
+        result = cur.execute(register_query,(username, email, gender, county, password))
+        if result:
+            conn.commit()
+            return render_template("login.html")
+        else:
+            msg = "Registration failed, try again"
+            return render_template("register.html", msg=msg)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = pymysql.connect("localhost", "root", "", "studentmgmt")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM register where username = %s and password = %s',
+                    (username,password))
+        result = cur.fetchone()
+        if result:
+            session['logged'] = True
+            session['username'] = result[0]
+            msg="logged in successfully"
+
+            return render_template("home.html", msg=msg)
+        else:
+            return render_template("login.html")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/home")
+def home():
+    if 'logged' in session:
+        return render_template("home.html")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/viewprofile")
+def viewprofile():
+    if 'logged' in session:
+        conn = pymysql.connect("localhost", "root", "", "studentmgmt")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM register where username = %s',(session['username']))
+        result = cur.fetchone()
+        return render_template("viewprofile.html", result=result)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/viewusers")
+def viewuser():
+    if 'logged' in session:
+        conn = pymysql.connect("localhost", "root", "", "studentmgmt")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM register ')
+        result = cur.fetchall()
+        return render_template("viewusers.html", result=result)
+    else:
+        return render_template("login.html")
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
