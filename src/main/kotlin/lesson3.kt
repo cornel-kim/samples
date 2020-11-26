549d2ebe746aa7ed0e007dea4992ab1dd011cb50
cornel-kim <cornelkemboi@yahoo.com>
2020-11-26 13:34:52 +0300

    first

---
+++ src/main/kotlin/lesson3.kt
@@ -0,0 +1,21 @@
+//when, while, for, break and continue
+
+//when loop
+fun main(){
+    val number:Int = -10
+//    when{
+//        number < 0 -> println("negative number")
+//        number == 0 -> println("number is zero")
+//        number > 0 -> println("number is positive")
+//    }
+    if(number < 0){
+        println("number is negative")
+    }else if(number == 0){
+        println("number is zero")
+    }
+    else{
+        println("number is negative")
+    }
+
+
+}
