3c62c7a49e88e1b14beec52e18f6ad0e8b3f53dd
cornel-kim <cornelkemboi@yahoo.com>
2020-12-10 09:19:36 +0300

    First commit

---
+++ lesson2a.py
@@ -0,0 +1,32 @@
+#functions- loops while and for loop, lists and tuples
+#code reusability
+#code cleanliness
+#leads to object oriented programming
+
+def my_name():#function declaration
+    print("My name is John")
+
+
+my_name()#fucntion invoke/triggre or use or call
+
+
+def addition():
+    a = 10
+    b = 15
+    c = a + b
+    print("The sum of a and b is ", c)
+
+
+addition()
+
+
+
+def temperature():
+    temp = 37.2
+    if temp > 37.2:
+        print("You have high fever")
+    else:
+        print("You body temperature is normal")
+
+
+temperature()
