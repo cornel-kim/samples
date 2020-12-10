32d2553f8305ac86e3b835c901579dab0f3fc6d7
cornel-kim <cornelkemboi@yahoo.com>
2020-12-10 09:35:52 +0300

    First commit

---
+++ lesson2c.py
@@ -0,0 +1,11 @@
+#while and for loop
+#print all numbers from 0 to 10
+x = 2
+while x < 10:
+    print("Your numbers in range of 10 are:",x)
+    x = x + 3#x++ or x+=1-incrementing values
+
+
+#for loop
+for x in range(0,10,2):
+    print("For loop looping:",x)
