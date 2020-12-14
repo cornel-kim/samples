40944f159a2a324303303005f61b7e0d35c00a4d
cornel-kim <cornelkemboi@yahoo.com>
2020-12-14 10:57:14 +0300

    firstcommit

---
+++ app/src/main/java/com/example/myappdec/JKUATActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myappdec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JKUATActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_j_k_u_a_t)
+    }
+}
--- app/src/main/java/com/example/myappdec/MainActivity.kt
+++ app/src/main/java/com/example/myappdec/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myappdec
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //intents, implicit and explicit intents- implicit intents starts activities on application
+        //while explicit intents start various activity in tha application
+        btnUON.setOnClickListener {
+            val i = Intent(this, UONActivity::class.java)
+            startActivity(i)
+        }
+        btnJKUAT.setOnClickListener {
+            val i = Intent(this, JKUATActivity::class.java)
+            startActivity(i)
+        }
     }
 }
