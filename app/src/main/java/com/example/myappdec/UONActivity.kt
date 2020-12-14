88ce188c103f12a69925804f2a6805f11280adde
cornel-kim <cornelkemboi@yahoo.com>
2020-12-14 10:37:40 +0300

    firstcommit

---
+++ app/src/main/java/com/example/myappdec/UONActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myappdec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UONActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_u_o_n)
+    }
+}
