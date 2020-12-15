53a773896cc23b00548b4f2d7a2fc985471833e3
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 15:31:14 +0300

    First commit

---
+++ lesson1a.py
@@ -0,0 +1,12 @@
+#check if  a value is true or false
+# check if a value is negative or positive
+y = int(input("Please input your value:"))#allow users to input various data
+if y > 0:
+    print("Y is a positive number")
+
+else:
+    print("Y is a negative number")
+
+
+
+
