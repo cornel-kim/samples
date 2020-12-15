ef6e02aa2c95754d599bd358165cb6537b38cfda
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 15:36:22 +0300

    First commit

---
+++ lesson1b.py
@@ -0,0 +1,6 @@
+#control statements- if else, if else elif and nested if else
+mark = int(input("Please input students marks:"))
+if mark > 0 and mark < 101:
+    print("You have added a valid mark")
+else:
+    print("Input a valid mark")
