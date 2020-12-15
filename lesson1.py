6bccf273043176d36f06dc2c550f24ab72461ceb
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 15:20:44 +0300

    First commit

---
+++ lesson1.py
@@ -0,0 +1,27 @@
+#operands and operators
+#operands = and, or, >, <, >=,<=, and ==
+#operators +, - , / , % , = , and *
+
+#addition
+number1 = 4
+number2 = 8
+sum_numbers = number1 + number2#addition sign for sum
+print(sum_numbers)
+
+
+x = 76
+y = 20
+div_values = x / y#division
+print(div_values)
+
+
+x = 76
+y = 20
+div_values = x % y#modulus-find the remainder value
+print(div_values)
+
+
+f = 5
+g = 3
+h = f * g
+print(h)
