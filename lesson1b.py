d4191e528f4ff6ed0e35e3bb02e4a060b931d54d
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 15:48:49 +0300

    First commit

--- lesson1b.py
+++ lesson1b.py
@@ -1,23 +1,23 @@
 #control statements- if else, if else elif and nested if else
-# mark = int(input("Please input students marks:"))
-# if mark > 0 and mark < 101:
-#     print("You have added a valid mark")
-# else:
-#     print("Input a valid mark")
+mark = int(input("Please input students marks:"))
+if mark > 0 and mark < 101:
+    print("You have added a valid mark")
+else:
+    print("Input a valid mark")
 
 
 
 #nested if else statement
-# user_type = str(input("Please input your user type i. e Teacher or student:"))
-# if user_type =="student":
-#     print("Should do exam")
-#     fee = str(input("Check if fee is cleared:"))
-#     if fee == "cleared":
-#         print("All clear to do exams")
-#     else:
-#         print("You havent cleared you fee hence not eligible")
-# else:
-#     print("You are not a student")
+user_type = str(input("Please input your user type i. e Teacher or student:"))
+if user_type =="student":
+    print("Should do exam")
+    fee = str(input("Check if fee is cleared:"))
+    if fee == "cleared":
+        print("All clear to do exams")
+    else:
+        print("You havent cleared you fee hence not eligible")
+else:
+    print("You are not a student")
 
 
 
@@ -35,7 +35,7 @@ elif student_mark > 0 and student_mark<=200:
 else:
     print("Please input valid mark")
 
-
+#check control statements, using for loop and while loop
 
 
 
