0f771991dbcf70d1c6b6def9cc5d4e34e73260f6
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 13:51:58 +0300

    firstcommit

--- app/src/main/java/com/example/myappdec/JKUATActivity.kt
+++ app/src/main/java/com/example/myappdec/JKUATActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myappdec
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_j_k_u_a_t.*
 
 class JKUATActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_j_k_u_a_t)
+        btnCall.setOnClickListener {
+            val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:072345678"))
+            startActivity(i)
+        }
+        btnFindUs.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("geo:-1.1017058,37.0054426z=15"))
+            startActivity(i)
+        }
     }
 }
