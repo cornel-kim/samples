221f178dc5ec0d23a2de8d899b396aea224867c1
cornel-kim <cornelkemboi@yahoo.com>
2020-12-15 15:08:10 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#It has extensive libraries for data science
+#it easy to use.
+#it is a progressive language
+#what is a variable- is a store of information, numbers, string, aphanumeric
+#data types, operands and operators
+#datatype-Intergers, floats, and string. Tuples, lists and dictionaries
+
+a = 10#a is a variable that store value 10
+print(type(a))
+print("My variable a of type int stores a value of ",a)
+
+b = "John"
+print(type(b))
+print("My variable b store a string value of ", b)
+
+
+c = 10.3
+print(type(c))
+print("My variable c stores a float of value ", c)
