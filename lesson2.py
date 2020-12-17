ef304f1d77500094b26f23a2394fb3a08eba88c6
cornel-kim <cornelkemboi@yahoo.com>
2020-12-17 14:44:11 +0300

    First commit

---
+++ lesson2.py
@@ -0,0 +1,17 @@
+#tuples, lists, and Dictionariorie
+#loops while and for loop
+#Python Function
+#Arrays-object are enclosed in the square bracktes[] or brackets()
+#Lists-ordered(indexed), mutable(can be changed), dynamic
+
+list1 = ["Peter", 4000, 32.5, "Kenya"]
+print(type(list1))
+print(list1)#access all the items of the list
+print(list1[0])#access the first item of the array or list
+print(list1[3])#access the last item of list1 by order
+print(list1[-1])#accessing item from last
+print(list1[:])#access all items of the list
+print(list1[:2])#access index 0 and 1
+print(list1[1:])#access items from index 1 to the last
+print(list1[2:3])#access item in-between index 2 and 3
+#accessing items by index is refered to as slicing
