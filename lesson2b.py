b0e902bcf9fcf3a2289a0e0a7897ef981ecdc15f
cornel-kim <cornelkemboi@yahoo.com>
2020-12-17 15:12:45 +0300

    First commit

---
+++ lesson2b.py
@@ -0,0 +1,19 @@
+#lists and tuples and dictionaries
+schools = ["Nairobi", "Chebisas", "Nakuru High", "Kenya"]
+print(schools)
+#access individual item in an array- implement loops
+#start for loop
+for school in schools:
+    print(school)
+#for loop increments by 1 by default
+
+for x in range(1, 10, 2):
+    print("Values in range of 1 to 10:", x)
+
+
+
+#while loop
+while_var = 1
+while while_var < 10:
+    print("The while loop values :", while_var)
+    while_var = while_var + 2
