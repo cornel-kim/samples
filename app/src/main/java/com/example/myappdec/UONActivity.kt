983501b1f97efa2ef24901f59ffd9ce9c7f18fbf
cornel-kim <cornelkemboi@yahoo.com>
2021-01-14 05:41:43 +0300

    firstcommit

--- app/src/main/java/com/example/myappdec/UONActivity.kt
+++ app/src/main/java/com/example/myappdec/UONActivity.kt
@@ -25,15 +25,12 @@ class UONActivity : AppCompatActivity() {
                 "The only institution of higher learning in Kenya for a long time, the University of Nairobi responded to the national regional and Africa's high level manpower training needs by developing and" +
                 " evolving strong, diversified academic programmes and specializations in sciences, applied sciences, technology, humanities, social sciences and the arts. To date, the range of programmes offered number approximately two hundred.\n" +
                 "\n" +
-                "Through module II (continuing education) programmes, invaluable opportunity has been opened to hundreds of Kenyans and non-Kenyans, on a paying basis, who meet university admission requirements, but who have not been able to access university education due to restricted intake into the regular programmes that is determined by limited resource allocation by Government.\n" +
-                "\n" +
-                "In this effort, the location of the seven campuses of the University in the capital city and its environs and facilitating easy access, has been an asset that has seen the university increasingly become the busy hub and citadel of academic activity at all times.\n" +
-                "\n" +
-                "In addition to the regular and evening and weekend programmes, classes are conducted at the University's Extra Mural Centres located at the country's provincial headquarters.\n" +
-                "\n" +
+
                 "The university is proud of its distinguished record of achievements in teaching, research, development and consultancy while strategizing for a greater future as the centre of academic, research and professional excellence.</p>"+
                 "</body>"
 
             ,HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
+//create an corona virus application, to show symptoms on one activity, prevention on another one, modes transmission, Contact page,
+// open web for daily recordings
